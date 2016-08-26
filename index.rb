047687e7d464be97213e8e3efb6d62e6ac727b03
narekchang <narek.changlyan@gmail.com>
2016-08-27 04:17:35 +0700

    Voice event

--- index.rb
+++ index.rb
@@ -3,8 +3,13 @@ token = '257482587:AAHkdako3TGWc1--PKt3evunrjZcz3lIBlw'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-    when '/start'
-      bot.api.sendMessage(chat_id: message.chat.id, text: "Hello1, #{message.from.first_name}")
+      when '/start'
+        bot.api.sendMessage(chat_id: message.chat.id, text: "Hi, #{message.from.first_name}")
+      end
+
+    if message.voice
+       bot.api.sendMessage(chat_id: message.chat.id, text: "Voice duration, #{message.voice.duration}")
     end
+
   end
 end
