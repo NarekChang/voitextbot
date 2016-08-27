2ec247c8a23ae0c97096a3f951c6009ccd0c6c05
narekchang <narek.changlyan@gmail.com>
2016-08-27 13:09:11 +0700

    Remove token

--- index.rb
+++ index.rb
@@ -1,5 +1,5 @@
 require 'telegram/bot'
-token = '257482587:AAHkdako3TGWc1--PKt3evunrjZcz3lIBlw'
+token = 'token'
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
@@ -9,10 +9,10 @@ Telegram::Bot::Client.run(token) do |bot|
 
     if message.voice
 
-      id = message.voice.file_id
-      j = bot.api.getFile(file_id: id)
-      path = j["result"]["file_path"]
-      f_url = "https://api.telegram.org/file/bot#{token}/#{path}"
+      v_id = message.voice.file_id
+      v_file = bot.api.getFile(file_id: id)
+      v_path = v_file["result"]["file_path"]
+      f_url = "https://api.telegram.org/file/bot#{token}/#{v_path}"
 
       bot.api.sendMessage(chat_id: message.chat.id, text: "Voice dwlnd link, #{f_url}")
 
